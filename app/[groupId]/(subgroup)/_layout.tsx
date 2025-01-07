305784a933db061bbce8d847440beac8c7cbdb94
Jean Desauw <desauwjean@gmail.com>
2025-01-07 11:17:32 +0100

    subgroup/layout make it one line

--- app/[groupId]/(subgroup)/_layout.tsx
+++ app/[groupId]/(subgroup)/_layout.tsx
@@ -3,8 +3,6 @@ import { Stack } from "expo-router";
 
 export default function SubgroupLayout() {
     return <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen 
-            name="item/index" 
-        />
+        <Stack.Screen name="item/index" />
     </Stack>;
 }
