3ab0a073663f55852614743f150eec7f8cdb7053
Jean Desauw <desauwjean@gmail.com>
2025-01-07 11:33:51 +0100

    update and improve the subgroupPage

--- app/[groupId]/(subgroup)/item/index.tsx
+++ app/[groupId]/(subgroup)/item/index.tsx
@@ -4,13 +4,12 @@ import { useEffect } from "react";
 import { Pressable, Text, View } from "react-native";
 
 const SubgroupItemPage = () => {
-  const { groupId, subgroupId } = useLocalSearchParams<{
+  const { groupId } = useLocalSearchParams<{
     groupId: string;
-    subgroupId: string;
   }>();
 
   useEffect(() => {
-    console.log("SubgroupItemPage mounted");
+    console.log("SubgroupItemPage mounted with groupId:", { groupId });
     return () => {
       console.log("SubgroupItemPage unmounted");
     };
@@ -44,7 +43,11 @@ const SubgroupItemPage = () => {
           fontWeight: "bold",
         }}
       >
-        Subgroup ItemPage for subgroup {subgroupId}
+        Nested page: {groupId}/(subgroup)/item/index
+      </Text>
+
+      <Text style={{ color: "grey", fontSize: 15, marginTop: 15, marginBottom: 5 }}>
+        Those links are not working, although the path is correct: "/{groupId}"
       </Text>
 
       <Link style={sharedButtonStyle} href={`/${groupId}`}>
@@ -53,17 +56,44 @@ const SubgroupItemPage = () => {
 
       <Pressable
         onPress={() => {
-          console.log("pressed");
-          router.push(`/${groupId}`);
+          if (groupId !== undefined && groupId !== null) {
+            const groupUrl = `/${groupId}`;
+            console.log('router.push to url:', groupUrl);
+            router.push(`/${groupId}`);
+          } else {
+            console.log('groupId is undefined');
+          }
         }}
         style={sharedButtonStyle}
       >
         <Text style={sharedTextStyle}>Pressable - Go to Group page</Text>
       </Pressable>
 
+      <Text style={{ color: "grey", fontSize: 15, marginTop: 15, marginBottom: 5 }}>
+        These links are working:
+      </Text>
+
       <Link style={sharedButtonStyle} href="/">
-        <Text style={sharedTextStyle}>Go to Home page</Text>
+        <Text style={sharedTextStyle}>Link - Go to Home page</Text>
       </Link>
+
+      <Link style={sharedButtonStyle} href="/wrong/path">
+        <Text style={sharedTextStyle}>Link - Go to a wrong path (404)</Text>
+      </Link>
+
+      <Pressable 
+        onPress={() => {
+          router.back();
+        }}
+        style={{...sharedButtonStyle, opacity: router.canGoBack() ? 1 : 0.5}}
+        disabled={!router.canGoBack()}
+      >
+        <Text style={sharedTextStyle}>Pressable - Go back</Text>
+      </Pressable>
+      
+      <Text style={{ color: "grey", fontSize: 15, marginTop: 15, marginBottom: 5 }}>
+        <Text style={{ fontWeight: "bold", color: "white" }}>Note:</Text> removing the "[groupId]/(subgroup)/_layout.tsx" file makes the links work. 🤷🏼‍♂️
+      </Text>
     </View>
   );
 };
